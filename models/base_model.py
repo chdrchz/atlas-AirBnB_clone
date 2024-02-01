de6a35e3371a8310f0aca0d9ab2cb0e7e2b4b418
chdrchz <saavannaa.rose@gmail.com>
2024-02-01 13:53:42 -0600

    i put my import for storage in the wrong spot

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class named BaseModel"""
 from datetime import datetime
-from models import storage
 import uuid
+import models
 
 
 class BaseModel:
@@ -10,6 +10,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Public instantation method"""
+        from models import storage
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
