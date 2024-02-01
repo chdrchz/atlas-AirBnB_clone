fbf63f0a965daca4691fa4fe3fb6e484d3cb8821
chdrchz <saavannaa.rose@gmail.com>
2024-02-01 14:36:33 -0600

    adding engine folder and class FileStorage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This module defines a class named FileStorage"""
+import json
+
+
+class FileStorage():
+    """This class serializes and deserializes instances"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """This method returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """This method sets keys and values for a dictionary"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """This method serializes __objects to the JSON file"""
+        serial_objects = {}
+        for key, obj in self.__objects.items():
+            serial_objects[key] = obj.to_dict()
+        with open(self.__file_path, "w") as f:
+            json.dump(serial_objects, f)
+
+    def reload(self):
+        """This method deserializes the JSON file to __objects"""
+        try: 
+            with open(self.__file_path, "r", encoding="utf-8") as f:
+                data = json.load(f)
+                for key, obj_dict in data.items:
+                    obj = eval(obj_dict['__class__'])(**obj_dict)
+        except:
+            pass
