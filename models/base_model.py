bbd4af8928d1b02e29ab1bd13cde4bac6bd5c86d
chdrchz <saavannaa.rose@gmail.com>
2024-02-01 14:58:46 -0600

    changing location of storage import

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 """This module defines a class named BaseModel"""
 from datetime import datetime
 import uuid
-from models import storage
 
 
 class BaseModel:
@@ -10,6 +9,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Public instantation method"""
+        from models import storage
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -35,6 +35,7 @@ class BaseModel:
 
     def save(self):
         """Updates the updated_at attribute with the current datetime"""
+        from models import storage
         self.updated_at = datetime.now()
         storage.save()
     
