f11014c7a7516af692d00d75a93801a25a3d8fee
chdrchz <saavannaa.rose@gmail.com>
2024-02-01 14:57:24 -0600

    adding storage bits to BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """This module defines a class named BaseModel"""
 from datetime import datetime
 import uuid
+from models import storage
 
 
 class BaseModel:
@@ -26,6 +27,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """String representation method"""
@@ -34,6 +36,7 @@ class BaseModel:
     def save(self):
         """Updates the updated_at attribute with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
     
     def to_dict(self):
         """Returns a dict of all instances created"""
