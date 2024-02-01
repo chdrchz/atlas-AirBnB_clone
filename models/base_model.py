e50607813eaf48714b3318c45b6a420657bbc999
chdrchz <saavannaa.rose@gmail.com>
2024-02-01 12:42:08 -0600

    testing change to to_dict method

--- models/base_model.py
+++ models/base_model.py
@@ -23,10 +23,8 @@ class BaseModel:
     
     def to_dict(self):
         """Returns a dict of all instances created"""
-        result = self.__dict__.copy()
-        result['__class__'] = type(self).__name__
-        if 'created_at' in result and isinstance(result['created_at'], datetime):
-            result['created_at'] = result['created_at'].isoformat()
-        if 'updated_at' in result and isinstance(result['updated_at'], datetime):
-            result['updated_at'] = result['updated_at'].isoformat()
-        return result
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
