0846d4d4421b0b3c19608eb589ebe35d82ea013d
chdrchz <saavannaa.rose@gmail.com>
2024-02-01 12:45:25 -0600

    replacing print with return in __str__ method

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,7 @@ class BaseModel:
 
     def __str__(self):
         """String representation method"""
-        print("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
+        return("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
 
     def save(self):
         """Updates the updated_at attribute with the current datetime"""
