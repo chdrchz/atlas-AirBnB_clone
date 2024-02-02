f9ee88be38412d74808f459cf8b78c942bcb3736
chdrchz <saavannaa.rose@gmail.com>
2024-02-02 13:43:47 -0600

    adding storage import to BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -8,6 +8,8 @@ class BaseModel:
     """This is the base class named BaseModel"""
     def __init__(self, *args, **kwargs):
         """Public instantation method"""
+        from models import storage
+        
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
