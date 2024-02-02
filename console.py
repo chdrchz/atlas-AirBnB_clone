1e5acc9fdbb1a475c33d02f78a4c7984d99351ef
chdrchz <saavannaa.rose@gmail.com>
2024-02-02 14:01:49 -0600

    imprting classes to console

--- console.py
+++ console.py
@@ -4,6 +4,12 @@ import cmd
 import sys
 from models.base_model import BaseModel
 from models.__init__ import storage
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 class HBNBCommand(cmd.Cmd):
