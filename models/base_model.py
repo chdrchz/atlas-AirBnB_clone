8f60a9751334be46bc66b6e788daa0f02cfb08c6
chdrchz <saavannaa.rose@gmail.com>
2024-02-02 13:45:31 -0600

    adding models import to whole module

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """This module defines a class named BaseModel"""
 from datetime import datetime
 import uuid
+import models
 
 
 class BaseModel:
@@ -9,7 +10,7 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Public instantation method"""
         from models import storage
-        
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
