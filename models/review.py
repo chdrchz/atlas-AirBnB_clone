24ee7554dfd0a9b426b70490974808df69e2cb2c
chdrchz <saavannaa.rose@gmail.com>
2024-02-02 13:28:16 -0600

    adding amenity, city, place, review, and state classes

---
+++ models/review.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines a class named Review that inherits from BaseModel
+"""
+from base_model import BaseModel
+
+
+class Review(BaseModel):
+    """This is a class Review that inherits from BaseModel"""
+    place_id = ""
+    user_id = ""
+    text = ""
