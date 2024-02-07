fedb0e6d6a83667333fc1b0e598c4321836f405b
ally2211 <amendieta.va@gmail.com>
2024-02-07 17:31:27 -0600

    pycodestyle for base_model

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,7 @@ class BaseModel:
                 self.id = str(uuid.uuid4())
                 self.created_at = datetime.now()
                 self.updated_at = datetime.now()
-        else: 
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -37,14 +37,15 @@ class BaseModel:
 
     def __str__(self):
         """String representation method"""
-        return("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
+        return ("[{}] ({}) {}"
+                .format(type(self).__name__, self.id, self.__dict__))
 
     def save(self):
-        """Updates the updated_at attribute with the current datetime and saves"""
+        """Updated_at attribute with the current datetime and saves"""
         from models import storage
         self.updated_at = datetime.now()
         storage.save()
-    
+
     def to_dict(self):
         """Returns a dict of all instances created"""
         data = self.__dict__.copy()
