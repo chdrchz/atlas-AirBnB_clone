3a7360f37f611741547832805efec9cf2d246dd1
ally2211 <amendieta.va@gmail.com>
2024-02-07 18:42:41 -0600

    add test_state.py

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module is a set of unit tests for User"""
+import unittest
+import os
+from models.engine.file_storage import FileStorage
+from models.state import State
+
+my_model = State()
+
+
+class TestState(unittest.TestCase):
+    """Class test for State"""
+
+    def test_state(self):
+        """Unit tests for State"""
+        state = State()
+        self.assertTrue(hasattr(state, 'name'))
+        self.assertEqual(state.name, "")
