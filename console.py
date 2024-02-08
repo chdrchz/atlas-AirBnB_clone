6352f496b8ad95d0d79c5f8c8b69fb60eb81f770
ally2211 <amendieta.va@gmail.com>
2024-02-07 19:15:45 -0600

    add missing comma

--- console.py
+++ console.py
@@ -30,7 +30,7 @@ class HBNBCommand(cmd.Cmd):
     dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
     types = {
              'first_name': str, 'number_rooms': int, 'number_bathrooms': int,
-             'max_guest': int, 'price_by_night': int, 'email': str
+             'max_guest': int, 'price_by_night': int, 'email': str,
              'latitude': float, 'longitude': float, 'last_name': str,
              'password': str
             }
